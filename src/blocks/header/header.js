7b698f70151e1c65719293d5f34e80d00c2f7775
Evgeny <djek.uait1998@gmail.com>
2023-04-06 21:58:32 +0300

    first commit

---
+++ src/blocks/header/header.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $(".header__burger").click(function(){
+        $(".header__button").toggleClass("header__button--hide")
+        $(".header__links").toggleClass("header__links--open")
+        $(".header__line:nth-child(1)").toggleClass("header__line--first")
+        $(".header__line:nth-child(2)").toggleClass("header__line--last")
+    })
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let myBtns=document.querySelectorAll('.header__button');
+    myBtns.forEach(function(btn) {
+
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('header__button--active'));
+          btn.classList.add('header__button--active');
+        });
+ 
+    });
+
+});
---
+++ src/blocks/main/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let myBtns=document.querySelectorAll('.main__btn');
+    myBtns.forEach(function(btn) {
+
+        btn.addEventListener('click', () => {
+          myBtns.forEach(b => b.classList.remove('main__btn--active'));
+          btn.classList.add('main__btn--active');
+        });
+ 
+    });
+
+});
